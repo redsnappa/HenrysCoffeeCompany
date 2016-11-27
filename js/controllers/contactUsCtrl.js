3aada00a17b7666e2b7c24193f397be39579bb03
Dan Thomson <dan.j.thomson@googlemail.com>
2016-11-27 19:15:21 +0000

    Contact Us errors

--- js/controllers/contactUsCtrl.js
+++ js/controllers/contactUsCtrl.js
@@ -5,25 +5,27 @@ angular.module('hccApp').controller('ContactUsController', ['$scope','$http',fun
     $scope.message = "";
     $scope.email = "";
     $scope.submitted = false;
+    $scope.sending = false;
     $scope.result = "";
 
     $scope.submitForm = function(){
 
         var hostname="http://ec2-54-154-172-41.eu-west-1.compute.amazonaws.com:8080/hello-henry";
         $scope.submitted = true;
+        $scope.sending = true;
         $http({
             method : "POST",
             url : hostname,
             data: { name: $scope.name, message: $scope.message,  email: $scope.email}
         }).then(function mySuccess(response) {
-            $scope.submitted = false;
+            $scope.sending = false;
             $scope.name = "";
             $scope.message = "";
             $scope.email = "";
             $scope.result = "Message was sent!";
         }, function myError(response) {
-            $scope.submitted = false;
             $scope.result = "Message couldn't be send at this time, try again later";
+            $scope.sending = false;
         });
     }
 
